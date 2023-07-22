14e24cb20402d1c6ff443fb4e5bc51947d8ceaf3
emre sert <volverainc@gmail.com>
2023-07-22 17:28:33 +0300

    detect systems

--- src/App.js
+++ src/App.js
@@ -3,25 +3,29 @@ import 'react-toastify/dist/ReactToastify.css';
 import Hero from './components/hero';
 import Skills from './components/skills';
 import Profile from './components/profile';
-import "./styles/dark.css"
+import "./styles/dark.css";
 import Projects from './components/projects';
 import Footer from './components/footer';
-import "./styles/responsive.css"
+import "./styles/responsive.css";
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 function App(props) {
-  const { theme } = props
+  const { theme } = props;
   const { i18n } = useTranslation();
   const [language, setLanguage] = useState(localStorage.getItem('translated') || 'en');
+
   useEffect(() => {
-    i18n.changeLanguage(language)
-    const storedLanguage = localStorage.getItem('translated');
-    if (storedLanguage && storedLanguage !== i18n.language) {
-      i18n.changeLanguage(storedLanguage);
-      setLanguage(storedLanguage);
+    const systemLanguage = navigator.language.split('-')[0];
+
+    if (systemLanguage !== i18n.language) {
+      i18n.changeLanguage(systemLanguage);
+      setLanguage(systemLanguage);
+      localStorage.setItem('translated', systemLanguage);
+    } else {
+      i18n.changeLanguage(language);
     }
-  }, [i18n, language])
+  }, [i18n]);
 
   return (
     <div className={`App ${theme}`}>
--- src/components/header.js
+++ src/components/header.js
@@ -1,52 +1,79 @@
 import { useTranslation } from 'react-i18next';
 import { useEffect, useState } from 'react';
 import { Toggle } from '../Toggle';
+
 const Header = () => {
-    const [language, setLanguage] = useState(localStorage.getItem('translated') || 'en');
-    const { i18n } = useTranslation();
+  const [language, setLanguage] = useState(localStorage.getItem('translated') || 'en');
+  const { i18n } = useTranslation();
 
-    const handleLanguageChange = (event) => {
-        i18n.changeLanguage(event.target.value);
-        localStorage.setItem("translated", event.target.value);
-        setLanguage(event.target.value);
+
+  useEffect(() => {
+    const systemLanguage = navigator.language.split('-')[0];
+
+    if (systemLanguage !== i18n.language) {
+      i18n.changeLanguage(systemLanguage);
+      setLanguage(systemLanguage);
+      localStorage.setItem('translated', systemLanguage);
+    }
+  }, [i18n]);
+
+  const handleLanguageChange = (event) => {
+    const selectedLanguage = event.target.value;
+    i18n.changeLanguage(selectedLanguage);
+    setLanguage(selectedLanguage);
+    localStorage.setItem('translated', selectedLanguage);
+  };
+
+  const systemDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const [theme, setTheme] = useState(localStorage.getItem('dark mode') || (systemDark ? 'dark' : 'light'));
+
+  const toggleTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+  };
+
+  useEffect(() => {
+    document.body.className = theme;
+    localStorage.setItem('dark mode', theme);
+  }, [theme]);
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem('dark mode');
+    if (savedTheme) {
+      setTheme(savedTheme);
+    }
+  }, []);
+
+  useEffect(() => {
+    const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    const handleDarkModeChange = (e) => {
+      setTheme(e.matches ? 'dark' : 'light');
     };
+    darkModeMediaQuery.addListener(handleDarkModeChange);
+    return () => darkModeMediaQuery.removeListener(handleDarkModeChange);
+  }, []);
 
-    const [theme, setTheme] = useState(localStorage.getItem('dark mode') || 'default')
-
-    const toggleTheme = () => {
-        if (theme === 'light') {
-            setTheme('dark');
-        } else {
-            setTheme('light');
-        }
-    };
-
-    useEffect(() => {
-        document.body.className = theme;
-        localStorage.setItem('dark mode', theme);
-      }, [i18n, language, theme]);
-
-    return (
-        <header>
-            <div className="logo">
-                <a href="/" style={{ textDecoration: "none" }}><h1>emresert</h1></a>
-            </div>
-            <div className="header-items">
-                <div>
-                    <button className='language-button' value={i18n.language === 'en' ? 'tr' : 'en'} onClick={handleLanguageChange}>
-                        {i18n.language === 'en' ? 'TÜRKÇE' : 'ENGLISH'}
-                    </button>
-                </div>
-                <div>
-                    <Toggle
-                        toggled={true}
-                        onClick={toggleTheme}
-                    />
-                    <span className='dark-span'>{theme === "dark" ? "LIGHT MODE" : "DARK MODE"}</span>
-                </div>
-            </div>
-        </header>
-    )
+  return (
+    <header>
+      <div className="logo">
+        <a href="/" style={{ textDecoration: "none" }}><h1>emresert</h1></a>
+      </div>
+      <div className="header-items">
+        <div>
+          <button className='language-button' value={i18n.language === 'en' ? 'tr' : 'en'} onClick={handleLanguageChange}>
+            {i18n.language === 'en' ? 'TÜRKÇE' : 'ENGLISH'}
+          </button>
+        </div>
+        <div>
+          <Toggle
+            toggled={theme === 'dark'}
+            onClick={toggleTheme}
+          />
+          <span className='dark-span'>{theme === "dark" ? "LIGHT MODE" : "DARK MODE"}</span>
+        </div>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
